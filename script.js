980d9444bba858a2a589c91534ffd129f26e32bc
Dipu Singh <dipu.devops@gmail.com>
2026-02-27 18:14:03 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    alert("Ibrahim, get ready for an unforgettable journey through India! 🌏✈️");
+}
+
+// Console log for initialization
+console.log("Welcome Page for Ibrahim loaded successfully!");
